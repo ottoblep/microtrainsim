1aa96702cf28bc3a440dda4c436039c7598872c6
Severin Lochschmidt <seviron53@gmail.com>
2024-09-27 10:56:02 +0200

    Track train orientation

--- graph_heuristic_formulation.m
+++ graph_heuristic_formulation.m
@@ -4,7 +4,7 @@ params.infra = [ 0 50 10 0;
                  0 0 0 0];
 [params.edge_rows, params.edge_cols, params.edge_values] = find(params.infra);
 params.n_trains = 3;
-params.initial_pos = [2, 0.5; 3, 0.5; 1,0.1;];
+params.initial_pos = [2, 0.5, 1; 3, 0.5, 1; 1, 0.1, -1;];
 params.initial_speed = [0.2, 0.3, 0.4];
 params.n_timesteps = 100;
 params.min_separation = 5;
@@ -13,44 +13,50 @@ params.accel = 0.1;
 
 function sol = randomSolution(params)
     % Solution Array dimensions (train, timestep)
-    % Solution Array values (accelerate -1,0,1, direction 0-1)
+    % Solution Array values (acceleration -1,0,+1, direction 0-1)
     sol = rand(params.n_trains, params.n_timesteps - 1, 2);
     sol(:,:,1) = randi([-1,1], params.n_trains, params.n_timesteps - 1);
 end
 
 function traj = constructTrajectory(params, solution)
-    % solution dimensions (train, timestep) values (target_speed 0-1, direction 0-1)
-    % initial_pos dimensions (n_trains, 2) values (edge 0-n, position_on_edge 0-1
+    % solution dimensions (train, timestep) values (acceleration -1,0,+1, direction 0-1)
+    % initial_pos dimensions (n_trains, 2) values (edge 0-n, position on edge 0-1
     % initial_speed dimensions (n_trains, 1) values (velocity 0-1)
-    % trajectory dimensions (train, timestep) values (edge 0-n, position_on_edge 0-1)
+    % trajectory dimensions (train, timestep) values (edge 0-n, position on edge 0-1, train orientation on edge -1,1)
     traj(:, 1, :) = params.initial_pos;
-    speeds(:, 1) = params.initial_speed;
+
+    %% Calculate speed curves
+    % Speed is relative to train orientation
+    for i_train = 1:params.n_trains
+        speeds(i_train, :) = params.initial_speed(i_train) + cumtrapz(solution(i_train, :, 1)) * params.accel;
+        # Do not accelerate over maximum
+        speeds(i_train, speeds(i_train,:)>params.max_speed) = params.max_speed;
+        speeds(i_train,speeds(i_train,:)<-params.max_speed) = -params.max_speed;
+    end
 
     for timestep = 2:params.n_timesteps-1
         for i_train = 1:params.n_trains
-            %% Calculate speeds
-            new_speed = speeds(i_train, timestep-1) + params.accel * solution(i_train, timestep, 1);
-            if abs(new_speed) < params.max_speed
-                speeds(i_train, timestep) = new_speed;
-            else
-                speeds(i_train, timestep) = speeds(i_train, timestep-1);
-            end
-
             %% Move trains
             % Start movement at previous edge
             traj(i_train, timestep, 1) = traj(i_train, timestep-1, 1);
-            remaining_movement = speeds(i_train, timestep);
+            remaining_movement = traj(i_train, timestep, 3) * speeds(i_train, timestep);
 
             abort = 10000;
             while abort > 0
                 if abort < 10
                    disp("oh oh");
                 end
-                current_edge_length = params.edge_values(traj(i_train, timestep, 1));
+                % DEBUG
+                current_edge = traj(i_train, timestep, 1);
+                current_progress = traj(i_train, timestep, 2);
+                current_direction = traj(i_train, timestep, 3);
+                %
 
+                current_edge_length = params.edge_values(traj(i_train, timestep, 1));
                 remaining_forward_length = (1 - traj(i_train, timestep, 2)) * current_edge_length;
                 remaining_backward_length = traj(i_train, timestep, 2) * current_edge_length;
 
+                remaining_movement = traj(i_train, timestep, 3) * abs(remaining_movement);
                 if abs(remaining_movement) < 1e-10
                     remaining_movement = 0;
                 end
@@ -65,30 +71,27 @@ function traj = constructTrajectory(params, solution)
                         remaining_movement = remaining_movement + remaining_backward_length;
                     end
 
+                    % Find next edge to enter
                     viable_next_edges = find((params.edge_rows == traversed_node) | (params.edge_cols == traversed_node));
                     viable_next_edges = viable_next_edges(viable_next_edges!=traj(i_train, timestep, 1));
                     if length(viable_next_edges) == 0
                         traj(i_train, timestep, 2) = (sign(remaining_movement) + 1) / 2;
                         remaining_movement = 0;
-                        speeds(i_train, timestep) = 0;
                         continue;
                     end
-
                     % Direction parameter decides next edge
                     next_edge = viable_next_edges(int32(floor(solution(i_train, timestep, 2) * length(viable_next_edges)) + 1));
 
                     % Move to new edge
                     traj(i_train, timestep, 1) = next_edge;
 
-                    % Set new edge position and movement direction
+                    % Set new edge position and train direction
                     if params.edge_rows(next_edge) == traversed_node
                         traj(i_train, timestep, 2) = 0;
-                        remaining_movement = abs(remaining_movement);
-                        speeds(i_train, timestep) = abs(speeds(i_train, timestep));
+                        traj(i_train, timestep, 3) = 1;
                     else
                         traj(i_train, timestep, 2) = 1;
-                        remaining_movement = -abs(remaining_movement);
-                        speeds(i_train, timestep) = -abs(speeds(i_train, timestep));
+                        traj(i_train, timestep, 3) = -1;
                     end
 
                     abort--;
