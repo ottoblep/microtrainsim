5a76dc68022e7c0d9486da65b4db2fc7094bab15
Severin Lochschmidt <seviron53@gmail.com>
2024-10-12 12:48:47 +0200

    Specify sparse allocation

--- random_planar_graph.m
+++ random_planar_graph.m
@@ -1,5 +1,5 @@
 function adj = random_planar_graph(n)
-    %adj = spalloc(n, n);
+    % adj = spalloc(n, n, 6*n*n); % for finite planar graphs the average degree is strictly less than 6
     del = delaunay(randn(n, 2));
     for i_triangle = 1:size(del, 1)
         adj(del(i_triangle, 1),del(i_triangle, 2)) = 1;
