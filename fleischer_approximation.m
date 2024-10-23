627262d3a9380f908f825a31025dd2c87ef67cdb
Severin Lochschmidt <seviron53@gmail.com>
2024-10-23 18:43:56 +0200

    Implement fleischer approximation for max mcnf

--- fleischer_approximation.m
+++ fleischer_approximation.m
@@ -25,59 +25,62 @@ demand_pair_idxs = demand_pair_idxs{demand_pair_idxs{:,1}~=demand_pair_idxs{:,2}
 
 demand_paths = cell(k_demand_pairs, 1);
 for i_demand_pair = 1:k_demand_pairs
-     demand_paths{i_demand_pair} = allpaths(g_network, demand_pair_idxs(i_demand_pair, 1), demand_pair_idxs(i_demand_pair, 2));
+     [~, demand_paths{i_demand_pair}] = allpaths(g_network, demand_pair_idxs(i_demand_pair, 1), demand_pair_idxs(i_demand_pair, 2));
 end
-n_demand_path_sizes = cellfun('size', demand_paths, 2);
+n_demand_path_sizes = cellfun('size', demand_paths, 1);
 n_demand_paths = sum(n_demand_path_sizes);
 e_accuracy = 0.01;
 
+% L maximum number of arcs in augmenting path
+L = max(n_demand_path_sizes);
+
 % delta is initial dual problem path length
-delta = (m_edges / (1 - e_accuracy))^(-1/e_accuracy);
+delta = (1 + e_accuracy) / ((1 + e_accuracy) * L)^(1/e_accuracy);
 
 % Initialize decision vars for primal and dual problem
 x = zeros(1, n_demand_paths);
-l = delta / edge_capacities;
+l = ones(1, m_edges) * delta;
 
 % Dual objective function D(l) = sum(u(e)l(e))
 D_l = @(l_arg) sum(edge_capacities.*l_arg');
 demand_remaining = zeros(1,k_demand_pairs);
 
 n_phases = 0;
-while D_l(l) < 1
+for i = 1:log(((1+e_accuracy)/delta)) / log(1+e_accuracy)
      n_phases = n_phases + 1;
      for j = 1:k_demand_pairs
-          demand_remaining(j) = demands(j);
-          while D_l(l) < 1 & demand_remaining(j) > 0
-               % Shortest path in P_j using l
-               relevant_paths = paths{j};
-               P_idx = weightedShortestPath(relevant_paths, l);
-               P = relevant_paths{1,P_idx};
+          relevant_paths = demand_paths{j};
+          [P_idx, P_len] = weightedShortestPath(relevant_paths, l);
+          if P_idx == 0
+               continue;
+          end
+          P = relevant_paths{P_idx,1};
 
-               smallest_capacity_in_p = min(edge_capacities(P));
-               demand_to_assign_u = min([demand_remaining(j) smallest_capacity_in_p]);
-
-               demand_remaining(j) = demand_remaining(j) - demand_to_assign_u;
+          while P_len < min([1 delta*(1+e_accuracy)^i])
+               demand_to_assign_u = min(edge_capacities(P));
 
                global_path_idx = P_idx + sum(n_demand_path_sizes(1:j-1));
-
-               % Assign demand
                x(global_path_idx) = x(global_path_idx) + demand_to_assign_u;
 
                for i_path_edge = 1:numel(P)
                     global_edge_idx = P(i_path_edge);
                     l(global_edge_idx) = l(global_edge_idx) * (1 + (demand_to_assign_u * e_accuracy) / edge_capacities(global_edge_idx));
                end
+
+               [P_idx, P_len] = weightedShortestPath(relevant_paths, l);
+               P = relevant_paths{P_idx,1};
           end
      end
 end
 
 % Scale result
-result = x / (log(1 / delta) / log(1 + e_accuracy));
+result = x / (log((1 + e_accuracy)/delta) / log(1 + e_accuracy));
 
-function shortest_path_idx = weightedShortestPath(paths, edge_weights)
+function [shortest_path_idx, best_length] = weightedShortestPath(paths, edge_weights)
      best_length = Inf;
+     shortest_path_idx = 0;
      for i_path = 1:numel(paths)
-          path = paths{1,i_path};
+          path = paths{i_path,1};
           length = 0;
           for j_edge_in_path = 1:size(path,2)
                length = length + edge_weights(path(j_edge_in_path));
