f3d4084af5f3acf6d30a70bdb54dc353d72ee02a
Severin Lochschmidt <seviron53@gmail.com>
2024-10-26 14:46:11 +0200

    Add matlab to flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,15 @@
 {
   description = "Nix Dev Shells";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  outputs = all@{ self, nixpkgs, ... }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nix-matlab = {
+      url = "gitlab:doronbehar/nix-matlab";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = all@{ self, nixpkgs, nix-matlab, ... }:
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
@@ -13,8 +20,15 @@
       # Utilized by `nix develop`
       devShell.x86_64-linux =
         pkgs.mkShell {
+          buildInputs = (with nix-matlab.packages.x86_64-linux; [
+            matlab
+            matlab-mlint
+            matlab-mex
+          ]);
+
+          shellHook = nix-matlab.shellHooksCommon;
+
           packages = with pkgs; with pkgs.python311Packages; [
-            octaveFull
             (python3.withPackages (python-pkgs: [
               pandas
               numpy
