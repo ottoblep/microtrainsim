6ae26e20dc77358688a2d077576b4ad9972a23df
Severin Lochschmidt <seviron53@gmail.com>
2024-11-08 21:09:27 +0100

    Improve braking curve calculation

--- matlab/testDwellTimeEnforcement.m
+++ matlab/testDwellTimeEnforcement.m
@@ -1,9 +1,10 @@
 timesteps = 100;
-points = [1 4 6 10] * 0.1 * timesteps;
-vals = [0.5 0.4 0.1 0.8];
-a_max = 0.3;
-v_init = 3;
-v_max = 10;
+interp_steps = 10;
+points = rand(1, interp_steps) * timesteps;
+vals = rand(1, interp_steps);
+a_max = 0.1;
+v_max = 3;
+v_init = rand(1,1) * v_max;
 dwell_time = 0.1 * timesteps;
 
 acceleration = interpolateSolutionCurve(points, vals, 1:timesteps) * a_max;
@@ -12,7 +13,7 @@ speeds(speeds>v_max) = v_max;
 speeds(speeds<-v_max) = -v_max;
 position = cumtrapz(speeds);
 
-initial_arrival_time = 0.4 * timesteps;
+initial_arrival_time = 0.5 * timesteps;
 
 % Braking at max acceleration until standstil leads to a quadratic position curve
 % We calculate the timestep to start braking so the train comes to a halt exactly at the stop 
@@ -23,25 +24,30 @@ initial_arrival_time = 0.4 * timesteps;
 stop_position = position(initial_arrival_time);
 approach_direction = sign(speeds(initial_arrival_time));
 
-
 pre_stop_timesteps = 1:initial_arrival_time;
 first_approach_idx = find(sign(speeds(1:initial_arrival_time)) ~= approach_direction, 1, 'last') + 1;
 approach_timesteps = first_approach_idx:initial_arrival_time;
 
 % Determine ideal start of braking
 d = position(approach_timesteps) - stop_position;
-closest_points = 1/2 * speeds(approach_timesteps).^2 / a_max - d;
-[~, start_braking_timestep] = min(abs(closest_points));
-start_braking_timestep =  start_braking_timestep + first_approach_idx;
-new_arrival_time = start_braking_timestep + (abs(speeds(start_braking_timestep)) / abs(a_max));
+[~, start_braking_timestep] = min(abs(0.5 * speeds(approach_timesteps).^2 / a_max - d));
+% Pessimistic rounding for discretization error
+start_braking_timestep =  floor(start_braking_timestep + first_approach_idx);
+new_arrival_time = ceil(start_braking_timestep + (abs(speeds(start_braking_timestep) / a_max)));
+exact_acceleration = abs(speeds(start_braking_timestep) / (new_arrival_time - start_braking_timestep));
 
 % Modify acceleration curve for ideal approach
-acceleration(start_braking_timestep: new_arrival_time) = -approach_direction;
-acceleration(new_arrival_time: new_arrival_time + dwell_time) = 0;
+acceleration(start_braking_timestep: new_arrival_time) = -approach_direction * (exact_acceleration / a_max);
+acceleration(new_arrival_time:new_arrival_time + dwell_time) = 0.5;
 speeds = v_init + cumtrapz(((acceleration * 2) - 1) * a_max);
+speeds(new_arrival_time:new_arrival_time + dwell_time) = 0;
 speeds(speeds>v_max) = v_max;
 speeds(speeds<-v_max) = -v_max;
 position = cumtrapz(speeds);
+
+v_error = speeds(new_arrival_time)
+p_error = position(new_arrival_time) - stop_position
+startpoint_error = min(abs(0.5 * speeds(approach_timesteps).^2 / a_max - d))
 
 clf;
 hold on;
@@ -50,7 +56,7 @@ scatter(first_approach_idx, 0,'DisplayName','first\_approach\_idx');
 scatter(initial_arrival_time, 0,'DisplayName','initial\_arrival\_time');
 scatter(start_braking_timestep, 0,'DisplayName','start\_braking\_timestep');
 scatter(new_arrival_time, 0,'DisplayName','new\_arrival\_time');
-plot(1:timesteps, acceleration,'DisplayName','accel');
+plot(1:timesteps, acceleration-0.5,'DisplayName','accel');
 plot(1:timesteps, speeds,'DisplayName','speed');
 plot(1:timesteps, position - stop_position,'DisplayName','dist\_to\_stop');
 legend();
