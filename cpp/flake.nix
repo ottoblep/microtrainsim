318b98956c120d0ba6201b74fc52173a0eab2566
Severin Lochschmidt <seviron53@gmail.com>
2024-11-26 16:11:29 +0100

    Add flake

---
+++ cpp/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "C++ DevShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # This is the list of architectures that work with this project
+      systems = [
+        "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"
+      ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # C++ Compiler is already part of stdenv
+            boost
+            catch2
+            cmake
+          ];
+        };
+      };
+    };
+}
