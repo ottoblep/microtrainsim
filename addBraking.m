dff7d17dae16693380daf6e2259997c98ed29c5c
Severin Lochschmidt <seviron53@gmail.com>
2024-12-13 16:31:46 +0100

    Return to simplified and universal directional braking

--- addBraking.m
+++ addBraking.m
@@ -1,10 +1,13 @@
 function [v_targets, start_braking_timestep, end_braking_timestep] = addBraking(params, global_speeds, v_targets, edge_transition, immediate_departure, hold_until_timestep, braking_goal_speed)
     %% Returns a subset of v_targets modified so that train will reach a certain velocity before reaching the new edge specified in edge_transition
-    % Moves speed into legal envelope regardless of direction
+    % Moves speed to braking_goal speed regardless of direction changes 
 
     assert(abs(edge_transition.speed) >= abs(braking_goal_speed));
     % global_speeds ranges from start of simulation until edge_transition.timestep
     assert(numel(global_speeds) == edge_transition.timestep);
+
+    % Ensure directionality
+    braking_goal_speed = sign(edge_transition.node_traversal_direction) * abs(braking_goal_speed);
 
     v_target_timesteps = v_targets(:, 1);
     v_target_values = v_targets(:, 2);
@@ -22,16 +25,12 @@ function [v_targets, start_braking_timestep, end_braking_timestep] = addBraking(
 
     % Place speed target point of 0 at the start of braking or not accelerating
     v_target_timesteps(end+1) = start_braking_timestep;
-    if abs(global_speeds(start_braking_timestep + 1)) < abs(braking_goal_speed)
-        v_target_values(end+1) = global_speeds(start_braking_timestep + 1);
-    else
-        v_target_values(end+1) = sign(global_speeds(start_braking_timestep + 1)) * braking_goal_speed;
-    end
+    v_target_values(end+1) = braking_goal_speed;
 
     v_targets = [v_target_timesteps v_target_values];
 end
 
-function [start_braking_timestep, end_braking_timestep, final_braking_goal_speed] = findBrakingTimestep(params, global_speeds, edge_transition, braking_goal_speed)
+function [start_braking_timestep, end_braking_timestep] = findBrakingTimestep(params, global_speeds, edge_transition, braking_goal_speed)
     %% Find start_braking_timestep that undershoots the position at edge_transition.timestep the littlest while braking with max_accel starting at start_braking_timestep
     
     global_trajectory = zeros(1, edge_transition.timestep);
@@ -43,13 +42,15 @@ function [start_braking_timestep, end_braking_timestep, final_braking_goal_speed
     % Candidate timesteps are the steps where braking actually takes effect 
     % start_braking_timestep (speed target point) is 1 timestep earlier due to propagation delay
     candidate_timesteps = 2:edge_transition.timestep;
-    abs_diff_to_legal_speed = max(abs(global_speeds(candidate_timesteps)) - abs(braking_goal_speed), 0);
-    required_braking_time = ceil(abs_diff_to_legal_speed / params.max_accel);
-    goal_speed_reached_at = candidate_timesteps + required_braking_time' - 1;
-    dist_covered_while_braking = global_speeds(candidate_timesteps) .* (required_braking_time - 1) ...
-                                 - 0.5 * max(required_braking_time - 2, 0) .* max(required_braking_time - 1, 0) * params.max_accel ...
-                                 + rem(abs_diff_to_legal_speed, params.max_accel);
-    dist_remaining_after_braking = abs(global_trajectory(candidate_timesteps) - target_position) - dist_covered_while_braking';
+    abs_diff_to_target_speed = abs(global_speeds(candidate_timesteps) - braking_goal_speed);
+    required_braking_time = ceil(abs_diff_to_target_speed / params.max_accel);
+    goal_speed_reached_at = candidate_timesteps + required_braking_time';
+    accel_direction = sign(braking_goal_speed - global_speeds(candidate_timesteps));
+    % We need a general integral here including zero crossings that can happen
+    dist_covered_while_braking = global_speeds(candidate_timesteps) .* required_braking_time ...
+                                 + accel_direction .* (0.5 * (required_braking_time + 1) .* required_braking_time * params.max_accel);
+    % Overshot = negative dist remaining
+    dist_remaining_after_braking = target_position - global_trajectory(candidate_timesteps) - dist_covered_while_braking';
     
     % Find latest possible braking point
     % Speed goal must be reached the step before the transition (p(n) = n(n-1) + v(n-1))
